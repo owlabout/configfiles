b298fdc5df8102405a9dbf7a9faf856154c4cfd6
Lukas Ehnle <lukas@owlabout.de>
2020-07-04 19:50:43 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: false,
+	trailingComma: "es5",
+	bracketSpacing: true, // { foo: bar }
+	arrowParens: "avoid",
+	htmlWhitespaceSensitivity: "ignore",
+	// svelte-specific
+	svelteSortOrder: "scripts-markup-styles",
+}
